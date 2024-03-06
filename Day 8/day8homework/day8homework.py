09b64ddc8584967f91cb00d8cde487fbb22e65af
sandrochkhaidze <156508839+sandrochkhaidze@users.noreply.github.com>
2024-03-07 00:10:47 +0400

    Add files via upload

---
+++ Day 8/day8homework/day8homework.py
@@ -0,0 +1,5 @@
+User1_Age = int(input("Enter your age Here"))
+User2_Age = int(input("Enter your age Here"))
+User3_Age = int(input("Enter your age Here"))
+Amount = 3   # Because there are 3 men in total
+print((User1_Age + User2_Age + User3_Age) // Amount)
